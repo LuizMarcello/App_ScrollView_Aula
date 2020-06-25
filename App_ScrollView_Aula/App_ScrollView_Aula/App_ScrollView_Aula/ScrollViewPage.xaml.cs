22672af01f3c52647957795b80d557e408494079
luizmarcelloxamarin <luiztelemicro@hotmail.com>
2020-06-24 21:35:59 -0300

    24.06.2020_21:35

--- App_ScrollView_Aula/App_ScrollView_Aula/App_ScrollView_Aula/ScrollViewPage.xaml.cs
+++ App_ScrollView_Aula/App_ScrollView_Aula/App_ScrollView_Aula/ScrollViewPage.xaml.cs
@@ -16,42 +16,42 @@ namespace App_ScrollView_Aula
         {
             InitializeComponent();
 
-            var box1 = new BoxView
-            {
-                Color = Color.Blue,
-                HeightRequest = 500,
-                WidthRequest = 200
-            };
+            //var box1 = new BoxView
+            //{
+            //    Color = Color.Blue,
+            //    HeightRequest = 500,
+            //    WidthRequest = 200
+            //};
 
-            var box2 = new BoxView
-            {
-                Color = Color.Yellow,
-                HeightRequest = 500,
-                WidthRequest = 200
-            };
+            //var box2 = new BoxView
+            //{
+            //    Color = Color.Yellow,
+            //    HeightRequest = 500,
+            //    WidthRequest = 200
+            //};
 
-            var box3 = new BoxView
-            {
-                Color = Color.Red,
-                HeightRequest = 500,
-                WidthRequest = 200
-            };
+            //var box3 = new BoxView
+            //{
+            //    Color = Color.Red,
+            //    HeightRequest = 500,
+            //    WidthRequest = 200
+            //};
 
-            var stacklayout = new StackLayout
-            {
-                Spacing = 10,
-                Children =
-                {
-                    box1, box2, box3
-                }
-            };
+            //var stacklayout = new StackLayout
+            //{
+            //    Spacing = 10,
+            //    Children =
+            //    {
+            //        box1, box2, box3
+            //    }
+            //};
 
-            var scrollview = new ScrollView
-            {
-                Content = stacklayout
-            };
+            //var scrollview = new ScrollView
+            //{
+            //    Content = stacklayout
+            //};
 
-            Content = scrollview;
+            //Content = scrollview;
         }
     }
 }
